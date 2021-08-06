bcfbeb3d00e6e900752f54eac951282b0d626765
Edgar Ramos <edgar.ramos@mozcalti.com>
2021-08-06 18:17:02 -0500

    Commit inicial

---
+++ src/main/java/me/d3249/demo/kurggervacunas/KurggerVacunasApplication.java
@@ -0,0 +1,13 @@
+package me.d3249.demo.kurggervacunas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurggerVacunasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurggerVacunasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/d3249/demo/kurggervacunas/KurggerVacunasApplicationTests.java
@@ -0,0 +1,13 @@
+package me.d3249.demo.kurggervacunas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurggerVacunasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
